4ce7d5deed648e1bd562a0b72ee74e6a4ab88317
martinludwigtrust-cpu <martinludwigtrust@gmail.com>
2026-06-16 17:00:00 +0200

    Delete api/debug.js

--- api/debug.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const key = process.env.ANTHROPIC_API_KEY;
-  res.status(200).json({
-    key_set: !!key,
-    key_length: key ? key.length : 0,
-    key_start: key ? key.slice(0, 15) : 'NOT SET',
-    key_end: key ? key.slice(-4) : 'NOT SET'
-  });
-}
